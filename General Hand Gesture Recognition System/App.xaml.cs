23f040cc2e25ed48604dba35d9cb990b0c49dbd4
Kazhunter <moh95_1@yahoo.com>
2016-07-02 01:00:48 +0200

    These are VS2013 files needed for project also

---
+++ General Hand Gesture Recognition System/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace General_Hand_Gesture_Recognition_System
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
